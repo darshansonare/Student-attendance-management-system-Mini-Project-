e134166531a24b967f51d41bd4b59a53a152de1b
Darshan Sonare <104987000+darshansonare@users.noreply.github.com>
2024-05-05 16:25:01 +0530

    Successfully uploaded

--- login.js
+++ login.js
@@ -1,57 +1,66 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCx5dUs6Pr2X5h1sfYXzMJ_eHo2zEudcLg",
-    authDomain: "attendance-management-sy-3fa44.firebaseapp.com",
-    databaseURL: "https://attendance-management-sy-3fa44-default-rtdb.firebaseio.com",
-    projectId: "attendance-management-sy-3fa44",
-    storageBucket: "attendance-management-sy-3fa44.appspot.com",
-    messagingSenderId: "985008312602",
-    appId: "1:985008312602:web:cfd537a98ad1c5e40b1ca3",
-    measurementId: "G-9BLJD0VEWG"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  
-  function isValidEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  }
-  
-  function signup(event) {
-    event.preventDefault();
-    const nameInput = document.getElementById("name1").value;
-    const emailInput = document.getElementById("email").value;
-    const passwordInput = document.getElementById("password").value;
-    const dobInput = document.getElementById("dob").value;
-  
-    if (!isValidEmail(emailInput)) {
-      alert('Please enter a valid email address');
-      return;
+
+function tryLogin()
+{
+    let un=$("#txtUsername").val();
+    let pw=$("#txtPassword").val();
+    if(un.trim()!=="" && pw.trim()!="")
+    {
+       //make an ajax call
+       $.ajax({
+        url:"ajaxhandler/loginAjax.php",
+        type:"POST",
+        dataType:"json",
+        data:{user_name:un,password:pw,action:"verifyUser"},
+        beforeSend:function(){
+            //if you want to do something just 
+            //before making the call
+            //alert("about to make an ajax call");
+            $("#diverror").removeClass("applyerrordiv");
+            $("#lockscreen").addClass("applylockscreen");
+            
+            //$("#errormessage").text("");
+        },
+        success:function(rv){
+            //if the ajax call was successful, 
+            //result will be in rv
+            //alert(JSON.stringify(rv));
+            $("#lockscreen").removeClass("applylockscreen");
+            if(rv['status']=="ALL OK")
+            {
+                document.location.replace("attendance.php");
+            }
+            else{
+                //alert(rv['status']);
+                $("#diverror").addClass("applyerrordiv");
+                $("#errormessage").text(rv['status']);
+            }
+        },
+        error:function(){
+            //if for some reason the call was unsuccessful
+            alert("oops something went wrong");
+        },
+       });
     }
-  
-    if (!nameInput || !emailInput || !passwordInput || !dobInput) {
-      alert('Please fill in all fields');
-      return;
-    }
-  
-    auth.createUserWithEmailAndPassword(emailInput, passwordInput)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        user.updateProfile({ displayName: nameInput })
-          .then(() => {
-            alert(`Sign-up successful! Welcome, ${user.displayName}`);
-            // Redirect to a blank page or perform additional actions
-          })
-          .catch((error) => {
-            const errorMessage = error.message;
-            alert(`Sign-up failed: ${errorMessage}`);
-          });
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        alert(`Sign-up failed: ${errorMessage}`);
-      });
-  }
-  
-  const form = document.getElementById('signup-form');
-  form.addEventListener('submit', signup);
+}
+//do everything only when the document is loaded
+$(function(e){
+    //capture the keyup event
+    $(document).on("keyup","input",function(e){
+        $("#diverror").removeClass("applyerrordiv");
+        //$("#errormessage").text("");
+        let un=$("#txtUsername").val();
+        let pw=$("#txtPassword").val();
+        if(un.trim()!=="" && pw.trim()!=="")
+        {
+          $("#btnLogin").removeClass("inactivecolor");
+          $("#btnLogin").addClass("activecolor");
+        }
+        else{
+            $("#btnLogin").removeClass("activecolor");
+            $("#btnLogin").addClass("inactivecolor");
+        }
+    }); 
+    $(document).on("click","#btnLogin",function(e){
+        tryLogin();
+    });
+});
